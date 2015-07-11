d2e7877a47fc19e4f528f52ab97d036ddc813f84
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2015-07-11 10:43:03 +0300

    rm manual angular bootstrapping

--- src/main.js
+++ src/main.js
@@ -4,53 +4,65 @@
 let query = require('./query')
 let tags = require('./tags')
 
+let http_err2str = function(err) {
+    if (err instanceof Error) {
+	return err.message
+    } else if (angular.isObject(err)) {
+	return `${err.config.method} ${err.config.url}: ${err.status} ${err.statusText}`
+    } else {
+	return err
+    }
+}
+
 let services = angular.module('proverbs.services', [])
-let app = angular.module('proverbs', [
+let app = angular.module('ProverbsApp', [
     'ngRoute',
     'proverbs.services'
 ])
 
 app.config(['$routeProvider', ($routeProvider) => {
     $routeProvider
-	.when('/about', {
-	    templateUrl: 'template.main',
-	    controller: "AboutCtrl"
-	})
 	.when('/search', {
-	    templateUrl: 'template.main',
+	    templateUrl: 'template.search',
 	    controller: "SearchCtrl"
 	})
 	.when('/tags', {
-	    templateUrl: 'template.main',
+	    templateUrl: 'template.tags',
 	    controller: "TagsCtrl"
 	})
 	.otherwise({
-	    redirectTo: '/about'
+	    redirectTo: '/search'
 	})
 }])
 
-let AboutCtrl = function($scope, $location) {
-    $scope.template_body = 'template.about'
-}
-app.controller('AboutCtrl', AboutCtrl)
-AboutCtrl.$inject = ['$scope', '$location']
+let MainCtrl = function($scope, $http) {
+    $scope.load_data = function() {
+	$http.get('data.json')
+	    .then( (r) => {
+		console.log(r)
+		$scope.ready = true
+	    })
+	    .catch( (err) => {
+		$scope.status = http_err2str(err)
+		console.log(err)
+	    });
+    }
 
-let SearchCtrl = function($scope, $location) {
-    $scope.template_body = 'template.search'
+    // Init
+    $scope.ready = false
+    $scope.load_data()
+}
+app.controller('MainCtrl', MainCtrl)
+MainCtrl.$inject = ['$scope', '$http']
+
+let SearchCtrl = function($scope) {
+    $scope.$parent.nav_current = 'search'
 }
 app.controller('SearchCtrl', SearchCtrl)
-SearchCtrl.$inject = ['$scope', '$location']
+SearchCtrl.$inject = ['$scope']
 
-let TagsCtrl = function($scope, $location) {
-    $scope.template_body = 'template.tags'
+let TagsCtrl = function($scope) {
+    $scope.$parent.nav_current = 'tags'
 }
 app.controller('TagsCtrl', TagsCtrl)
-TagsCtrl.$inject = ['$scope', '$location']
-
-
-let bootstrap_angular = function() {
-    angular.bootstrap(document, ['proverbs'])
-    console.info('angular bootstrap')
-}
-
-angular.element(document).ready(bootstrap_angular)
+TagsCtrl.$inject = ['$scope']
