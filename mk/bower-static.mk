b9dd7d2a3898fe7ae9a7c2eb5744c0e8deda7535
Alexander Gromnitsky <alexander.gromnitsky@gmail.com>
2015-07-16 23:00:04 +0300

    mk: don't break if there is no .min.js file

--- mk/bower-static.mk
+++ mk/bower-static.mk
@@ -7,15 +7,20 @@ bower-static.all := $(addprefix $(out)/$(bower-static.out)/, $(bower-static.file
 
 bower-static.src.suffix := $(filter $(bower-static.minify.suffix), $(sort $(suffix $(bower-static.files))))
 
+# $(call bower-static-suffix-change, FILE, SUFFIX)
+bower-static-suffix-change = $(patsubst %$(2),%.min$(2), $1)
+# $(call bower-static-get-file, FILE, SUFFIX)
+bower-static-get-file = $(or $(wildcard $(out)/bower_components/$(call bower-static-suffix-change,$1,$2)), $(out)/bower_components/$1)
+
 # $(call bower-static-get-source, FILE)
 ifdef bower-static.src.suffix
-bower-static-get-source = $(foreach suffix, $(bower-static.src.suffix),$(patsubst %$(suffix),%.min$(suffix), $1))
+bower-static-get-source = $(foreach suffix, $(bower-static.src.suffix),$(call bower-static-get-file,$1,$(suffix)))
 else
-bower-static-get-source = $1
+bower-static-get-source = $(out)/bower_components/$1
 endif
 
 define bower-static-rule =
-$$(out)/$$(bower-static.out)/$1: $$(out)/bower_components/$(call bower-static-get-source,$1)
+$$(out)/$$(bower-static.out)/$1: $(call bower-static-get-source,$1)
 	$$(copy)
 endef
 
